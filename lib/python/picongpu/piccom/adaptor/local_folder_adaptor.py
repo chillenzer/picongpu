be0b8a33dba0b2b99d9c66b5ca79ac56b4e26004
Julian Lenz <j.lenz@hzdr.de>
2025-09-17 11:12:06 +0200

    Switch to general get interface and add ids constraint

--- lib/python/picongpu/piccom/adaptor/local_folder_adaptor.py
+++ lib/python/picongpu/piccom/adaptor/local_folder_adaptor.py
@@ -6,7 +6,7 @@ License: GPLv3+
 """
 
 from enum import Enum
-from typing import Any
+from typing import Any, Iterable
 from random import sample
 from itertools import batched
 
@@ -118,7 +118,7 @@ def _apply_ordering(objs, ordering: Ordering):
     if ordering == Ordering.random:
         # for random access we actually have to hold a copy in memory
         objs = list(objs)
-        return sample(objs, k=len(objs))
+        return iter(sample(objs, k=len(objs)))
     raise ValueError(f"Applying the {ordering=} to {objs=} reached an unreachable branch.")
 
 
@@ -138,19 +138,44 @@ def _extract_full_metadata(obj):
     return obj
 
 
+class Retrievable(Enum):
+    full_metadata = "full_metadata"
+    ids = "ids"
+    parameters = "parameters"
+
+
+_RETRIEVABLE_TO_FUNCTION = {
+    Retrievable.full_metadata: _extract_full_metadata,
+    Retrievable.ids: _extract_id,
+    Retrievable.parameters: _extract_parameters,
+}
+
+
+class AllIds:
+    def __contains__(self, *args):
+        return True
+
+
+def _filter_by_ids(objs, ids):
+    return filter(lambda obj: _extract_id(obj) in ids, objs)
+
+
 class LocalFolderAdaptor:
     def __init__(self, database):
         self.database = database
 
     def _get_full_metadata(
         self,
+        ids: Iterable[str] | None = None,
         parameters: dict[str, Any] | None = None,
         status: dict[str, str] | None = None,
         ordering: Ordering = Ordering.arbitrary,
     ):
-        # might return an iterator which could be surprising to work with if you don't expect it
         return _apply_ordering(
-            _filter_by_parameters(_filter_by_status(self.database.find(), status), parameters), Ordering(ordering)
+            _filter_by_parameters(
+                _filter_by_status(_filter_by_ids(self.database.find(), ids or AllIds()), status), parameters
+            ),
+            Ordering(ordering),
         )
 
     def _get_batched_information(self, information, *args, batch_size: int | None = None, **kwargs):
@@ -159,21 +184,20 @@ class LocalFolderAdaptor:
             batch_size,
         )
 
-    def get_full_metadata(self, *args, batch_size: int | None = None, **kwargs):
-        # public interface writes a list into memory, so people don't accidentally exhaust the iterator
-        return _make_list(
-            self._get_batched_information(_extract_full_metadata, *args, batch_size=batch_size, **kwargs),
-            0 if batch_size is None else 1,
-        )
-
-    def get_ids(self, *args, batch_size: int | None = None, **kwargs):
-        return _make_list(
-            self._get_batched_information(_extract_id, *args, batch_size=batch_size, **kwargs),
-            0 if batch_size is None else 1,
-        )
-
-    def get_parameter_sets(self, *args, batch_size: int | None = None, **kwargs):
-        return _make_list(
-            self._get_batched_information(_extract_parameters, *args, batch_size=batch_size, **kwargs),
-            0 if batch_size is None else 1,
+    def get(
+        self,
+        retrievable: Retrievable | str,
+        *args,
+        return_as_iterators: bool = False,
+        batch_size: int | None = None,
+        **kwargs,
+    ):
+        retrievable = Retrievable(retrievable)
+        if not return_as_iterators:
+            return _make_list(
+                self.get(retrievable, *args, return_as_iterators=True, batch_size=batch_size, **kwargs),
+                0 if batch_size is None else 1,
+            )
+        return self._get_batched_information(
+            _RETRIEVABLE_TO_FUNCTION[retrievable], *args, batch_size=batch_size, **kwargs
         )
