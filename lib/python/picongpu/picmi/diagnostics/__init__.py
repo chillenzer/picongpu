f47c9b245fc36b779ff7568d54ecf274fc1881aa
Julian Lenz <j.lenz@hzdr.de>
2026-01-20 12:12:56 +0100

    Extract new submodule particle_functor

--- lib/python/picongpu/picmi/diagnostics/__init__.py
+++ lib/python/picongpu/picmi/diagnostics/__init__.py
@@ -12,10 +12,8 @@ from .energy_histogram import EnergyHistogram
 from .field_dump import DerivedFieldDump, NativeFieldDump
 from .macro_particle_count import MacroParticleCount
 from .particle_dump import ParticleDump
-from .particle_functor import ParticleFunctor
 from .phase_space import PhaseSpace
 from .timestepspec import TimeStepSpec
-from .unit_dimension import UnitDimension
 
 __all__ = [
     "BackendConfig",
@@ -27,10 +25,8 @@ __all__ = [
     "EnergyHistogram",
     "MacroParticleCount",
     "ParticleDump",
-    "ParticleFunctor",
     "NativeFieldDump",
     "DerivedFieldDump",
     "TimeStepSpec",
     "Checkpoint",
-    "UnitDimension",
 ]
--- share/picongpu/pypicongpu/examples/laser_wakefield/main.py
+++ share/picongpu/pypicongpu/examples/laser_wakefield/main.py
@@ -12,7 +12,7 @@ import numpy as np
 import sympy
 from picongpu import picmi, pypicongpu
 from picongpu.picmi.diagnostics import binning
-from picongpu.picmi.diagnostics.unit_dimension import I, L, M, T
+from picongpu.picmi.particle_functor.unit_dimension import I, L, M, T
 from scipy.constants import c, elementary_charge
 
 # set log level:
