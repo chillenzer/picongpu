c9e39342251e80a9194e6b6cb3faf46d8fae061b
Julian Lenz <j.lenz@hzdr.de>
2026-02-13 10:38:46 +0100

    Adjust test cases to pydantic PICMI standard

--- test/python/picongpu/quick/picmi/__init__.py
+++ test/python/picongpu/quick/picmi/__init__.py
@@ -1,6 +1,5 @@
 # flake8: noqa
 from .simulation import *  # pyflakes.ignore
-from .layout import *  # pyflakes.ignore
 from .distribution import *  # pyflakes.ignore
 from .gaussian_laser import *  # pyflakes.ignore
 from .grid import *  # pyflakes.ignore
