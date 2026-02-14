9a01d178088ae74d7c12e9ad93add7b6e7334590
Julian Lenz <j.lenz@hzdr.de>
2026-02-14 23:06:53 +0100

    Add tests for layouts

--- lib/python/picongpu/picmi/layout.py
+++ lib/python/picongpu/picmi/layout.py
@@ -5,9 +5,12 @@ Authors: Hannes Troepgen, Brian Edward Marre
 License: GPLv3+
 """
 
+from functools import partial
+from typing import Annotated, Self
 import numpy as np
 import picmistandard
-from pydantic import BaseModel, Field
+from pydantic import AfterValidator, BaseModel, Field, computed_field, model_validator
+from picmistandard.base import broadcast_validation
 
 from ..pypicongpu.species.operation.layout import OnePosition as PyPIConGPU_OnePosition
 from ..pypicongpu.species.operation.layout import Quiet, Random
@@ -25,16 +28,36 @@ class PseudoRandomLayout(picmistandard.PICMI_PseudoRandomLayout):
 
 
 class GriddedLayout(picmistandard.PICMI_GriddedLayout):
+    n_macroparticles_per_cell: list[int] = Field([0], init_var=False)
+
+    @model_validator(mode="after")
+    def _validate(self) -> Self:
+        self.n_macroparticles_per_cell = self.n_macroparticle_per_cell
+        return self
+
     def get_as_pypicongpu(self):
-        return Quiet(ppc=np.prod(self.n_macroparticles_per_cell), n_points=self.n_macroparticle_per_cell)
+        return Quiet(ppc=np.prod(self.n_macroparticle_per_cell), n_points=self.n_macroparticle_per_cell)
+
+    @computed_field
+    def in_cell_offsets(self) -> np.ndarray:
+        return (np.mgrid[*map(slice, self.n_macroparticles_per_cell)] + 0.5).reshape(
+            len(self.n_macroparticles_per_cell), -1
+        ).T / self.n_macroparticles_per_cell
 
 
 class OnePosition(BaseModel):
     n_macroparticles_per_cell: int = Field(gt=0, description="Number of particles per cell")
-    in_cell_offset: list[float] = Field(
+    in_cell_offset: Annotated[
+        list[float],
+        AfterValidator(
+            partial(
+                broadcast_validation,
+                condition=lambda v: v >= 0.0 and v < 1.0,
+                message="In-cell offset is relative to cell size and must be between 0 and 1.",
+            )
+        ),
+    ] = Field(
         default_factory=lambda: [0.0, 0.0, 0.0],
-        ge=0,
-        lt=1.0,
         min_length=3,
         max_length=3,
         description="Offset to cell origin where the particles are placed.",
